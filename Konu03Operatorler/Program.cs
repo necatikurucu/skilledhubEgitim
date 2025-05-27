21c66972a0d475ac8b747cc01ac41debcaa2de2d
necati kurucu <kn_1907@hotmail.com>
2025-05-27 22:01:47 +0300

    karar yapıları

--- Konu03Operatorler/Program.cs
+++ Konu03Operatorler/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Konu03Operatorler
+﻿using System.Runtime.CompilerServices;
+
+namespace Konu03Operatorler
 {
     internal class Program
     {
@@ -10,6 +12,64 @@
             int sayi2 = 4;
             int sayi3 = 5;
             Console.WriteLine();
+            Console.WriteLine($"Sayılar sayi1 : {sayi1} sayi2: {sayi2} sayi3: {sayi3}"); // string tırnağının önüne $ işateri koyarak string interpolasyonu yapıyoruz
+
+            Console.WriteLine("1234" + sayi3); // string ile int arasında + işlemi yapıldığında int stringe dönüştürülür ve string olarak birleştirilir
+
+            Console.WriteLine();
+            Console.WriteLine("hesaplama işlemleri:");
+            Console.WriteLine("sayi1 + sayi2 :" + (sayi1 + sayi2));
+            Console.WriteLine("sayi1 - sayi2 :" + (sayi1 - sayi2));
+            Console.WriteLine("sayi1 * sayi2 :" + (sayi1 * sayi2));
+            Console.WriteLine("sayi1 / sayi2 :" + (sayi1 / sayi2));
+            Console.WriteLine("sayi1 % sayi2 :" + (sayi1 % sayi2));
+
+            Console.WriteLine();
+
+            Console.WriteLine("artırım ve azaltım operatörleri");
+            sayi2++; // bir değişkendeki değeri 1 artırmak için
+            Console.WriteLine("sayi2:"+ sayi2);
+            sayi2--; // bir değişkendeki değeri 1 azaltmak için
+            Console.WriteLine("sayi2:"+ sayi2);
+
+            Console.WriteLine("atama operatörleri");
+            Console.WriteLine($"Sayılar sayi1 : {sayi1} sayi2: {sayi2} sayi3: {sayi3}");
+            Console.WriteLine("sayi1 += sayi2 :" + (sayi1 += sayi2));
+            Console.WriteLine("sayi1 -= sayi2 :" + (sayi1 -= sayi2));
+            Console.WriteLine("sayi1 *= sayi2 :" + (sayi1 *= sayi2));
+            Console.WriteLine("sayi1 /= sayi2 :" + (sayi1 /= sayi2));
+            Console.WriteLine("sayi1 %= sayi2 :" + (sayi1 %= sayi2));
+
+            Console.WriteLine();
+
+            Console.WriteLine("ilişkisel operatörler"); // birden fazla değeri karşılaştırıp aralarındaki durumu öğrenmek için kullanırız
+
+            Console.WriteLine($"Sayılar sayi1 : {sayi1} sayi2: {sayi2} sayi3: {sayi3}");
+            Console.WriteLine("sayı 1 sayı 2 ye eşit mi ?" + (sayi1==sayi2));
+            Console.WriteLine("sayı 1 sayı 2 ye eşit değil mi  ?" + (sayi1 != sayi2));
+            Console.WriteLine("sayı 1 sayı 2 den büyük mü ?" + (sayi1 > sayi2));
+            Console.WriteLine("sayı 1 sayı 2 den küçük mü ?" + (sayi1 < sayi2));
+            Console.WriteLine("sayı 1 sayı 2 den büyük veya eşit mi ?" + (sayi1 >= sayi2));
+            Console.WriteLine("sayı 1 sayı 2 den küçük veya eşit mi ?" + (sayi1 <= sayi2));
+            Console.WriteLine();
+
+            Console.WriteLine("ternary operatörü"); // eğer karşılaştırma için 2 değer kullanacaksak karşılaştırmanın kısa yolu olarak kullanırız
+            Console.WriteLine("ternary : " + ((sayi1==sayi2) ? $"sayı 1({sayi1}) sayı 2({sayi2}) ye eşit" : $"sayı 1({sayi1}) sayı 2({sayi2}) ye eşit değil"));
+
+            Console.WriteLine();
+            Console.WriteLine("mantıksal operatörler");
+            Console.WriteLine("and & operatörü");
+
+            Console.WriteLine($"Sayılar sayi1 : {sayi1} sayi2: {sayi2} sayi3: {sayi3}");
+            Console.WriteLine("sayı 1 sayı 2 ye eşit mi ?" + (sayi1 == sayi2));
+            Console.WriteLine("sayı 1 sayı 2 den büyük mü ?" + (sayi1 > sayi2));
+            sayi1 = 4; // sayi1 değerini 4 yapalım ki sonuç true olsun
+            Console.WriteLine("sayı 1 sayı 2 ye eşitmi? ve sayı bir sayı 2 den küçükmü" +((sayi1==sayi2) && (sayi1<sayi2))); // iki değerinde true sonuç vermesi lazım 1 tanesi false olması durumunda  sonuç false olur
+
+            Console.WriteLine();
+            Console.WriteLine("veya || operatörü");
+            Console.WriteLine("sayı 1 sayı 2 ye eşitmi? veya sayı bir sayı 2 den küçükmü" + ((sayi1 == sayi2) || (sayi1 < sayi2))); 
+
 
         }
     }
---
+++ Konu04KararYapilari/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Konu04KararYapilari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("karar yapıları");
+
+            Console.WriteLine("bir sayı giriniz");
+            var sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi > 0) // eğer sayı 0 dan büyükse
+            {
+                Console.WriteLine("sayı pozitif");
+            }
+            else if (sayi < 0) // eğer sayı 0 dan küçükse
+            {
+                Console.WriteLine("sayı negatif");
+            }
+            else
+            {
+                Console.WriteLine("sayı sıfır");
+            }
+        }
+    }
+}
