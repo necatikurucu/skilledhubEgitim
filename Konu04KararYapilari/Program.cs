90fc22dffa6caa99d26f1fde5ca8e0a0349fe4dd
necati kurucu <kn_1907@hotmail.com>
2025-05-28 22:02:31 +0300

    konu05metotlar

--- Konu04KararYapilari/Program.cs
+++ Konu04KararYapilari/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("karar yapıları");
 
-            Console.WriteLine("bir sayı giriniz");
+            /*Console.WriteLine("bir sayı giriniz");
             var sayi = Convert.ToInt32(Console.ReadLine());
             if (sayi > 0) // eğer sayı 0 dan büyükse
             {
@@ -20,6 +20,80 @@
             {
                 Console.WriteLine("sayı sıfır");
             }
+
+            Console.WriteLine("kullanıcı adını giriniz");
+            string kullaniciAdi = Console.ReadLine();
+
+            Console.WriteLine("şifre giriniz");
+            string sifre = Console.ReadLine();
+
+            string veritabanındakikullanıcıadi = "admin";
+            string veritabanındakisifre = "123456";
+
+            if (kullaniciAdi == veritabanındakikullanıcıadi && sifre == veritabanındakisifre)
+            {
+                Console.WriteLine("giriş başarılı");            
+            }
+            else
+            {
+                Console.WriteLine("kullanıcı adı ve şifre hatalı");
+            }
+
+            int saat = DateTime.Now.Hour; // Şu anki saat bilgisini sistemden alı alır
+
+            if (saat < 18) // eğer saat 18 den küçükse
+            {
+                Console.WriteLine("iyi günler. saat : " + saat);
+            }
+            
+            else
+            {
+                Console.WriteLine("iyi akşamlar. saat : " + saat);
+            }
+
+            Console.WriteLine("ternary operatörü");
+
+            Console.WriteLine((saat < 18) ? "iyigünler. saat :" + saat :" iyi akşamlar. saat : " + saat);
+            */
+
+            Console.WriteLine("switch case yapısı ile akış kontrolü");
+
+            int ay = DateTime.Now.Month; // Şu anki ay bilgisini sistemden alır
+            Console.WriteLine("bulunduğumuz ay : " + ay);
+            switch (ay)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("kış mevsimi");
+                    Console.WriteLine();
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("ilkbahar mevsimi");
+                    Console.WriteLine();
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("yaz mevsimi");
+                    Console.WriteLine();
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("sonbahar mevsimi");
+                    Console.WriteLine();
+                    break;
+                default:
+                    Console.WriteLine("hata oluştu");
+                    break;
+
+
+            }
+
+
         }
     }
 }
---
+++ konu05Metodlar/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace konu05Metodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("metodlar");
+            Toplamayap(); // Metod çağrısı olmadan ekrana yazdırmaz
+            Toplamayap(50, 8);
+            Console.WriteLine("geriye değer döndüren metot :");
+            int sonuc = Toplamayap(10, 20, 30 ); // metot çağrısı ile geriye değer döndürülür.
+            Console.WriteLine("sonuç:" + sonuc);
+        }
+        static void Toplamayap()
+        {
+            Console.WriteLine(10 + 8);
+        }
+        static void Toplamayap(int sayi1, int sayi2)
+
+        {
+            // void olan metodlar geriye değer döndürmeyen metodlardır.
+            // aynı isimde metotlar farklı parametrelerle kullanulırsa buna overloading - aşırı yükleme denir.
+            // metot kullanımında kullanılan parametreler (sayi1 i sayi2) metodun kullanıldığı yerde metoda gönderilmek zorundadır , aksi halde program hata deni.
+            Console.WriteLine("sonuç:" + (sayi1 + sayi2));
+
+        }
+        static int Toplamayap(int sayi1, int sayi2, int sayi3)
+        {
+            //geriye değer döndüren metodlarda metot isminin önüne metodun geriye döndüreceği veri türü yazılır.
+            // metodun aldığı parametre ile geri dönüş veri türü farklı olabilir.aynı olmak zorunda değildir.
+            // yani bir metot paramere olarak int alırken geriye string bool vb veri türü döndürebilir.
+
+            return sayi1 + sayi2 + sayi3; // int olan metodlar geriye değer döndürür. return ile değer döndürülür
+        }
+    }
+}
+
