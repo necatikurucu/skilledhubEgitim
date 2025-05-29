ceb61d3f87cba3a493112603d1c84f42f959a04d
necati kurucu <kn_1907@hotmail.com>
2025-05-29 22:01:57 +0300

    konu06diziler

--- konu05Metodlar/Program.cs
+++ konu05Metodlar/Program.cs
@@ -5,11 +5,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("metodlar");
+            Console.WriteLine();
+            Console.Beep();
+            
+
             Toplamayap(); // Metod çağrısı olmadan ekrana yazdırmaz
             Toplamayap(50, 8);
             Console.WriteLine("geriye değer döndüren metot :");
             int sonuc = Toplamayap(10, 20, 30 ); // metot çağrısı ile geriye değer döndürülür.
             Console.WriteLine("sonuç:" + sonuc);
+            /*
+            Console.WriteLine("lütfen mail adresini giriniz");
+            var email =Console.ReadLine();
+            if(mailgonder(email)== false)
+            {
+                Console.WriteLine("mail adresi boş geçilemez");
+            }
+            else
+                Console.WriteLine("mail kabul edildi");
+            */
+            // KdvHesapla();
+            MatematikselMetotlar();
         }
         static void Toplamayap()
         {
@@ -31,6 +47,39 @@
             // yani bir metot paramere olarak int alırken geriye string bool vb veri türü döndürebilir.
 
             return sayi1 + sayi2 + sayi3; // int olan metodlar geriye değer döndürür. return ile değer döndürülür
+        }
+
+        static bool mailgonder(string mailAdresi)
+        {
+            if (!string.IsNullOrWhiteSpace(mailAdresi)) //Eğer mail adresi değişkenine bir değer girilmişse yani mail adresi boş değilse
+            {
+                return true; // geriye true değer döndür
+            }
+            else 
+                return false; // mail adresi değişkeni boşsa false döndür
+        }
+        static void KdvHesapla()
+        {
+            double fiyat, kdv, toplam;
+            Console.WriteLine("ürün fiyatını giriniz");
+            fiyat = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("kdv miktarını giriniz");
+            var kdvmiktari = Convert.ToDouble(Console.ReadLine());
+            kdv = fiyat * kdvmiktari / 100;
+            toplam = kdv + fiyat;
+            Console.WriteLine("ürün kdv tutarı :" + kdv +"TL");
+            Console.WriteLine("kdv dahil fiyatı : " + toplam + "TL");
+        }
+        static void MatematikselMetotlar()
+        {
+            //belirli matematiksel işlemleri yapan bazı metotlar
+            int max = Math.Max(10, 34); // x ve y sayılarının en büyüğünü tutar
+            int min = Math.Min(8, 45); // x ve y sayılarının en küçüğünü tutar.
+
+            double c = Math.Ceiling(12.67); // x sayısından büyük en küçük tam sayıyı tutar yukarı yuvarlama
+            double d = Math.Floor(11.87); // x sayısından küçük en büyük tam sayıyı tutar aşağı yuvarlama
+            Console.WriteLine("max : " + max + " min : " + min + " ceiling : " + c + " floor : " + d);
+
         }
     }
 }
---
+++ konu06Diziler/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace konu06Diziler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("diziler");
+            // dizi oluşturma
+            int[] sayi;
+            int[] ogrenciler = new int[6]; // ogrenciler isminde int veri tipi taşıyan  ve 7 elemandan oluşan bir dizi oluşturduk dizi boyutu artmaz azalmaz
+                                           //dizilerde indis denilen bir yapı vardır  ve içine eklenecek elemanlar 0 dan başlar
+            ogrenciler[0] = 100;           // diziye veri atama
+            ogrenciler[1] = 200;
+            ogrenciler[2] = 300;
+            ogrenciler[3] = 400;
+            ogrenciler[4] = 500;
+            ogrenciler[5] = 600;
+
+            Console.WriteLine("seçilen öğrenci no: " + ogrenciler[5]);
+
+
+        }
+    }
+}
