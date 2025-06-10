e35834e6fb19653f4aa88e3e1900a35cec1b3c9b
necati kurucu <kn_1907@hotmail.com>
2025-06-10 22:01:51 +0300

    Konu08SınıflarClasses

--- konu07Donguler/Program.cs
+++ konu07Donguler/Program.cs
@@ -36,6 +36,44 @@
 
             }
             while (toplam<12);
+
+            Console.WriteLine();
+
+            string[] kategoriler = { "bilgisayar", "elektronik", "cep telefonu"};
+
+            Console.WriteLine("4-Foreach döngüsü");
+            Console.WriteLine("kategoriler");
+            foreach (var kategori in kategoriler) // kategori dizin (ögesi) kategoriler dizisindeki her bir elemana karşılık geliyor            {
+            {
+                Console.WriteLine(kategori);   
+            }
+
+            Console.WriteLine();
+
+            // iç içe döngü kullanımı
+
+            string[] urunler = { "ürün 1", "ürün 2", "ürün 3" };
+            foreach (var kategori in kategoriler)
+            {
+                Console.WriteLine(kategori);
+                foreach (var urun in urunler) // her kategori için ürünler dizisindeki her bir elemana karşılık geliyor
+                {
+                    Console.WriteLine(urun);
+                }
+            }
+                Console.WriteLine();
+
+                Console.WriteLine("iç içe for döngüsü");
+                for (int i = 0; i < 5; i++) // i değişkeni 0 olarak atar ve i değeri 5 den küçük olana kadar döngü devam eder
+                {
+                    Console.WriteLine($"i değişkeninin değeri {i}");
+
+                    for (int k = 0; k < 3; k++) // k değişkeni 0 olarak atar ve j değeri 3 den küçük olana kadar döngü devam eder
+                    {
+                        Console.WriteLine($"\t k değişkeninin değeri {k}");
+                    }
+                }
+            
         }
     }
 }
---
+++ konu08SınıflarClasses/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace konu08SınıflarClasses
+{
+    internal class Ev // sınıf tanımlama
+    {
+        internal string sokakAdi;
+        internal int kapiNo;
+    }
+    /*   C# nesne yönelimli bir programlama dili olduğu için her şey sınıflar içinde tanımlanır.
+         sınıflara ve sınıf öğelerine erişim kısıtlanabilir veya belirli düzeylerde erişime izin verilebilir.
+         Öğelere erişimi kısıtlayan ya da yetki veren anahtar sözcüklere "Erişim Belirteçleri" (acces modifiers) denir.
+         *Erişim belirteçleri 4 ana sınıfa ayrılır
+         * public    : Erişim kısıtı yoktur, her yerden erişilebilir
+         * protected : Ait olduğu sınıftan ve o sınıftan türetilen sınıflardan erişilebilir
+         * internal  : Etkin projeye ait sınıflardan erişilebilir, onların dışında erişilemez
+         * private   : Yalnız bulunduğu sınıftan erişilebilir, dıştaki sınıflardan erişilemez
+         * Bir öğe yalnızca 1 erişim belirteci alabilir
+         * namespace erişim belirteci almaz çünkü o daima public tir
+         * Sınıflar public yada internal nitelemesi alabilirler ama protected yada private nitelemesi alamazlar
+         * enum erişim belirteci almaz çünkü daima public tir
+    */
+    public class deneme
+    {
+        public string Urunadi = "public öğeye herkes erişebilir";
+        protected class test // ait olduğu sınıftan ve o sınıftan türetilen sınıflardan erişilebilir
+        {
+            public string Urunadi;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sınıflar - classes");
+            #region Örnek1
+
+            
+            Ev ilkEv = new Ev(); // soyut bir yapı olan ev sınıfından somut bir nesne olan ilkev i oluşturduk
+            ilkEv.sokakAdi = "çiçek sk.";
+            ilkEv.kapiNo = 10;
+            Console.WriteLine("ilkEv sokak adi :" + ilkEv.sokakAdi);
+            Console.WriteLine("ilkEv kapi no : " + ilkEv.kapiNo);
+
+            Console.WriteLine();
+
+            Ev yazlikEv = new Ev();
+            yazlikEv.sokakAdi = "deniz sk.";
+            yazlikEv.kapiNo = 20;
+            Console.WriteLine("yazlikEv sokak adi :" + yazlikEv.sokakAdi);
+            Console.WriteLine("yazlikEv kapi no : " + yazlikEv.kapiNo);
+
+            Console.WriteLine();
+            
+            Ev koyEvi = new()
+            {
+                sokakAdi = "Pınar sk.", // bu şekilde de tanımlama yapabiliriz ; yerine , kullanmak gerekir
+                kapiNo = 34 
+            };
+            Console.WriteLine("koyEvi sokak adi :" + koyEvi.sokakAdi);
+            Console.WriteLine("koyEvi kapi no : " + koyEvi.kapiNo);
+            #endregion
+            Console.WriteLine();
+            #region Örnek2
+                        
+            Kullanici kullanici = new()
+            {
+                Adi = "Ahmet",
+                Soyadi = "Yılmaz",
+                Email = "ahmet@yilmaz.com",
+                Id = 1,
+                KullaniciAdi = "ahmety",
+                Sifre = "123456"
+            };
+
+            Kullanici necati = new()
+            {
+                Adi = "necati",
+                Soyadi = "kurucu",
+                Email = "kn_1907@hotmail.com",
+                Id = 32,
+                KullaniciAdi = "kn_1907",
+                Sifre = "1122335544"
+            };
+
+            Console.WriteLine("Kullanıcı bilgileri :");
+            Console.WriteLine("Adı : "+kullanici.Adi);
+            Console.WriteLine("SoyAdı : "+kullanici.Soyadi);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Adı : " + necati.Adi);
+            Console.WriteLine("SoyAdı : " + necati.Soyadi);
+            #endregion
+            /*
+            Console.WriteLine("Kullanıc Adınız :");
+            var kullaniciAdi = Console.ReadLine();
+            Console.WriteLine("Şifreniz :");
+            var sifre = Console.ReadLine();
+            if (kullaniciAdi == kullanici.KullaniciAdi && sifre == kullanici.Sifre)
+            {
+                Console.WriteLine("hoşgeldin : " + kullanici.Adi + " " + kullanici.Soyadi);
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı adı veya şifre hatalı!");
+            }
+            */
+            Console.WriteLine();
+
+            Araba araba = new()
+            {
+                Id = 1,
+                Marka = "Toyota",
+                Model = "Corolla",
+                KasaTipi = "Sedan",
+                YakitTipi = "Benzin",
+                VitesTipi = "Otomatik",
+                Renk = "Gümüş"
+            };
+            Console.WriteLine("Araç Bilgisi");
+            Console.WriteLine($" Marka : {araba.Marka} \n Model : {araba.Model} \n Renk : {araba.Renk}");
+
+            Console.WriteLine();
+
+            Araba araba2 = new()
+            {
+                Id = 2,
+                Marka = "Hyundai",
+                Model = "Excel",
+                KasaTipi = "Sedan",
+                YakitTipi = "Benzin + lpg",
+                VitesTipi = "Manuel",
+                Renk = "Beyaz"
+            };
+            Console.WriteLine("Araç Bilgisi");
+            Console.WriteLine($" Marka : {araba2.Marka} \n Model : {araba2.Model} \n Renk : {araba2.Renk}");
+
+            Console.WriteLine();
+        }
+    }
+    class Kullanici
+    {
+        internal int Id;
+        internal string KullaniciAdi;
+        internal string Sifre;
+        internal string Email;
+        internal string Adi;
+        internal string Soyadi;
+    }
+    class Araba
+    {
+        internal int Id;
+        internal string Marka;
+        internal string Model;
+        internal string KasaTipi;
+        internal string YakitTipi;
+        internal string VitesTipi;
+        internal string Renk;
+    } 
+
+    }
+
